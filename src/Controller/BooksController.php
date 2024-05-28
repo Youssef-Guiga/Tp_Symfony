d235c05de3a94d7fd67c3f278635f9f589e7b785
Arij <158512912+ArijMhenni@users.noreply.github.com>
2024-05-29 00:59:09 +0100

    hmd 5dem

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -1,8 +1,11 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Book;
 use App\Entity\Books;
+use App\Form\BookType;
 use App\Repository\BooksRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,6 +22,32 @@ return $this->render('book/index.html.twig', [
 'books' => $books,
 ]);
 }
+#[Route('/books/new', name: 'book_new')]
+public function new(Request $request, EntityManagerInterface $entityManager): Response
+{
+    $book = new Books();
+    $form = $this->createForm(BookType::class, $book);
+    $form->handleRequest($request);
 
+    if ($form->isSubmitted() && $form->isValid()) {
+        $entityManager->persist($book);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('book_index');
+    }
+
+    return $this->render('book/new.html.twig', [
+        'form' => $form->createView(),
+    ]);
+}
+
+#[Route('/books/delete/{id}', name: 'book_delete')]
+public function delete(Books $book, EntityManagerInterface $entityManager): Response
+{
+    $entityManager->remove($book);
+    $entityManager->flush();
+
+    return $this->redirectToRoute('book_index');
+}
 
 }
